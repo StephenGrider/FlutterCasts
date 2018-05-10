66e47530b781f8643b59fd7f3e874c18f93e932a
stephengrider <ste.grider@gmail.com>
2018-05-10 13:56:21 -0600

    clear terminals

--- prompter/lib/src/terminal.dart
+++ prompter/lib/src/terminal.dart
@@ -4,4 +4,12 @@ class Terminal {
   void printPrompt(String prompt) {
     stdout.writeln(prompt);
   }
+
+  void clearScreen() {
+    if (Platform.isWindows) {
+      stdout.write('\x1B[2J\x1B[0f');
+    } else {
+      stdout.write('\x1B[2J\x1B[3J\x1B[H');
+    }
+  }
 }
