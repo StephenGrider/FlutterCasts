05d309e620a838f9a812f886693b2daf4bfa5bf3
stephengrider <ste.grider@gmail.com>
2018-05-10 17:32:07 -0600

    option printing

--- prompter/example/main.dart
+++ prompter/example/main.dart
@@ -1,10 +1,15 @@
 import 'package:prompter_sg/src/terminal.dart';
+import 'package:prompter_sg/src/option.dart';
 
 void main() {
   var terminal = new Terminal();
 
-  terminal.clearScreen();
-  terminal.printPrompt('Hi there!');
-  var input = terminal.collectInput();
-  print('You just entered: $input');
+  var options = [
+    new Option('I want red', '#f00'),
+    new Option('I want blue', '#00f')
+  ];
+
+  terminal.printOptions(options);
+  var response = terminal.collectInput();
+  print(response);
 }
