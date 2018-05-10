76690f6ed245dccbd3429ea710e9cde20805ee3c
stephengrider <ste.grider@gmail.com>
2018-05-10 17:24:14 -0600

    print options

--- prompter/lib/src/terminal.dart
+++ prompter/lib/src/terminal.dart
@@ -1,8 +1,18 @@
 import 'dart:io';
+import 'option.dart';
 
 class Terminal {
   void printPrompt(String prompt) {
     stdout.writeln(prompt);
+  }
+
+  void printOptions(List<Option> options) {
+    for (var i = 0; i < options.length; i++) {
+      stdout.writeln('[$i] - ${options[i].label}');
+    }
+
+    stdout.writeln('Enter a choice\n');
+    stdout.write('>');
   }
 
   String collectInput() {
