02fcd792f344c83e4eed18008a31fe8ddd31f3c0
stephengrider <ste.grider@gmail.com>
2018-05-11 16:06:33 -0600

    binary q

--- prompter/lib/src/prompter.dart
+++ prompter/lib/src/prompter.dart
@@ -4,7 +4,11 @@ import 'terminal.dart';
 final Terminal _terminal = const Terminal();
 
 class Prompter {
-  ask(String prompt, List<Option> options) {
+  askBinary(String prompt) {
+    _terminal.printPrompt('$prompt (y/n)');
+  }
+
+  askMultiple(String prompt, List<Option> options) {
     _terminal.printPrompt(prompt);
     _terminal.printOptions(options);
 
@@ -13,7 +17,7 @@ class Prompter {
     try {
       return options[int.parse(input)].value;
     } catch (err) {
-      return ask(prompt, options);
+      return askMultiple(prompt, options);
     }
   }
 }
