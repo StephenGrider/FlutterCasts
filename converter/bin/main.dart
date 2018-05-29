c0066e4969c650aed1dd93ba3fc3b83f7928fab3
stephengrider <ste.grider@gmail.com>
2018-05-29 11:02:46 -0600

    exit

--- converter/bin/main.dart
+++ converter/bin/main.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
 import 'package:prompter_sg/prompter_sg.dart';
 
 void main() {
   final prompter = new Prompter();
 
-  prompter.askBinary('Are you here to convert an image?');
+  final choice = prompter.askBinary('Are you here to convert an image?');
+  if (!choice) {
+    exit(0);
+  }
 }
