48857a28b6460b12b63ff39f87ef91fcf06e28ff
stephengrider <ste.grider@gmail.com>
2018-05-29 11:50:20 -0600

    current dir

--- converter/bin/main.dart
+++ converter/bin/main.dart
@@ -8,4 +8,27 @@ void main() {
   if (!choice) {
     exit(0);
   }
+
+  final format = prompter.askMultiple('Select format:', buildFormatOptions());
+  // prompter.askMultiple('Select an image to convert:', buildFileOptions());
+  buildFileOptions();
+}
+
+List<Option> buildFormatOptions() {
+  return [
+    new Option('Convert to jpeg', 'jpeg'),
+    new Option('Convert to png', 'png')
+  ];
+}
+
+List<Option> buildFileOptions() {
+  // Get a reference to the current working directory
+  final currentDirectory = Directory.current;
+
+  // Find all the files and folders in this directory
+  final entities = currentDirectory.listSync();
+  print(entities);
+  // Look through that list and find only the *images*
+
+  // Take all the images and create an option object out of each
 }
