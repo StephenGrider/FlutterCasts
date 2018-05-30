57e57bb2f8001c98dcddc116e436d3e4c76cebd4
stephengrider <ste.grider@gmail.com>
2018-05-30 13:56:54 -0600

    customize app bar

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,15 +1,13 @@
-// I need to import a helper library
-// from flutter to get content on the screen
 import 'package:flutter/material.dart';
 
-// Define a 'main' function to run when our app starts
 void main() {
-  // Create a new text widget to show some text
-  // on the screen
   var app = MaterialApp(
-    home: Text('Hi there!'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+    ),
   );
 
-  // Take that widget and get it on the screen
   runApp(app);
 }
