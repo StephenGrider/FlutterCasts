070782ce6cedb6cf7ac4ee576c70af5419d54a40
stephengrider <ste.grider@gmail.com>
2018-05-30 14:13:30 -0600

    required param

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
-      floatingActionButton: FloatingActionButton(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Hi there!');
+        },
+      ),
       appBar: AppBar(
         title: Text('Lets see some images!'),
       ),
