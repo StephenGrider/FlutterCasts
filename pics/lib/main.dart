9775e8de6c31952be5ed254aea6583605354d89a
stephengrider <ste.grider@gmail.com>
2018-05-30 14:07:54 -0600

    named parameters

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
+      floatingActionButton: FloatingActionButton(),
       appBar: AppBar(
         title: Text('Lets see some images!'),
       ),
