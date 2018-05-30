8ee22cb148b226788cacc71850b52867763d53a5
stephengrider <ste.grider@gmail.com>
2018-05-30 16:15:52 -0600

    local import

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          print('Hi there!');
-        },
-      ),
-      appBar: AppBar(
-        title: Text('Lets see some images!'),
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }
