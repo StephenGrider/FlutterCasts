a9c31ec4026537e9acb39564c27b0950c5fbcb06
stephengrider <ste.grider@gmail.com>
2018-05-31 16:18:45 -0600

    containers

--- pics/lib/src/widgets/image_list.dart
+++ pics/lib/src/widgets/image_list.dart
@@ -10,7 +10,10 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (context, int index) {
-        return Image.network(images[index].url);
+        return Container(
+          margin: EdgeInsets.all(20.0),
+          child: Image.network(images[index].url),
+        );
       },
     );
   }
