8edf0199875b3077cf3e7ce7b6fc7bb897eb22cb
stephengrider <ste.grider@gmail.com>
2018-05-31 16:04:58 -0600

    showimage

--- pics/lib/src/widgets/image_list.dart
+++ pics/lib/src/widgets/image_list.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 import '../models/image_model.dart';
 
 class ImageList extends StatelessWidget {
-  List<ImageModel> images;
+  final List<ImageModel> images;
 
   ImageList(this.images);
+
+  Widget build(context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index) {
+        return Image.network(images[index].url);
+      },
+    );
+  }
 }
