fdc69350b22ab20af39e4d60f44e970ec3609f06
stephengrider <ste.grider@gmail.com>
2018-06-05 11:40:08 -0600

    added app

---
+++ login_stateful/lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Log Me In!',
+      home: Scaffold(
+        body: Text('Show a form here!'),
+      ),
+    );
+  }
+}
