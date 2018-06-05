0bb3cb49d81b6d2c2586620bb112409bd207251d
stephengrider <ste.grider@gmail.com>
2018-06-05 15:26:45 -0600

    validator

--- login_stateful/lib/src/screens/login_screen.dart
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -7,10 +7,13 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  final formKey = GlobalKey<FormState>();
+
   Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+        key: formKey,
         child: Column(
           children: [
             emailField(),
@@ -46,7 +49,9 @@ class LoginScreenState extends State<LoginScreen> {
     return RaisedButton(
       color: Colors.blue,
       child: Text('Submit!'),
-      onPressed: () {},
+      onPressed: () {
+        formKey.currentState.reset();
+      },
     );
   }
 }
