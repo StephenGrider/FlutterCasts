373ab92f0c93ec89274838321e8dce1f45bbf216
stephengrider <ste.grider@gmail.com>
2018-06-06 17:47:29 -0600

    textfield

---
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          emailField(),
+          passwordField(),
+          Container(margin: EdgeInsets.only(top: 25.0)),
+          submitButton(),
+        ],
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        hintText: 'you@example.com',
+        labelText: 'EMail Address',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Password',
+        labelText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Login'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
