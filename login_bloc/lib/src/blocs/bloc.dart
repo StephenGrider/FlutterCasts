08441c77531c7332371d5eb3024925ceba0ec384
stephengrider <ste.grider@gmail.com>
2018-06-07 15:20:17 -0600

    cleanup

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
+import 'validators.dart';
 
-class Bloc {
+class Bloc extends Object with Validators {
   final _email = StreamController<String>();
   final _password = StreamController<String>();
 
   // Add data to stream
-  Stream<String> get email => _email.stream;
-  Stream<String> get password => _password.stream;
+  Stream<String> get email => _email.stream.transform(validateEmail);
+  Stream<String> get password => _password.stream.transform(validatePassword);
 
   // Change data
   Function(String) get changeEmail => _email.sink.add;
   Function(String) get changePassword => _password.sink.add;
+
+  dispose() {
+    _email.close();
+    _password.close();
+  }
 }
---
+++ login_bloc/lib/src/blocs/validators.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+class Validators {
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains('@')) {
+      sink.add(email);
+    } else {
+      sink.addError('Enter a valid email');
+    }
+  });
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 3) {
+      sink.add(password);
+    } else {
+      sink.addError('Password must be at least 4 characters');
+    }
+  });
+}
