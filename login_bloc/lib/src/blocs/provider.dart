44ae012b132a28be59527f88bdc4c86c96aeb6c1
stephengrider <ste.grider@gmail.com>
2018-06-07 17:52:28 -0600

    constructor

--- login_bloc/lib/src/blocs/provider.dart
+++ login_bloc/lib/src/blocs/provider.dart
@@ -4,6 +4,8 @@ import 'bloc.dart';
 class Provider extends InheritedWidget {
   final bloc = Bloc();
 
+  Provider({Key key, Widget child}) : super(key: key, child: child);
+
   bool updateShouldNotify(_) => true;
 
   static Bloc of(BuildContext context) {
