a5f29680b9ef7db6610d18f8edcf6d5c993863cc
stephengrider <ste.grider@gmail.com>
2018-06-07 12:42:23 -0600

    getters

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,3 +1,14 @@
 import 'dart:async';
 
-class Bloc {}
+class Bloc {
+  final _email = StreamController<String>();
+  final _password = StreamController<String>();
+
+  // Add data to stream
+  Stream<String> get email => _email.stream;
+  Stream<String> get password => _password.stream;
+
+  // Change data
+  Function(String) get changeEmail => _email.sink.add;
+  Function(String) get changePassword => _password.sink.add;
+}
