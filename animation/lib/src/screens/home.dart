70c3d2287101c2398bfcbaa7801a369ba08a6a05
stephengrider <ste.grider@gmail.com>
2018-06-11 17:08:47 -0600

    initstate

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
 
 class Home extends StatefulWidget {
   HomeState createState() => HomeState();
 }
 
 class HomeState extends State<Home> {
-  Widget build(context) {}
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+  }
+
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation!'),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
+  }
 }
---
+++ animation/lib/src/widgets/cat.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget {
+  build(context) {
+    return Image.network(
+      'https://i.imgur.com/QwhZRyL.png',
+    );
+  }
+}
