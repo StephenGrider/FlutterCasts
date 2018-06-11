b2385cb623a42490d6e5ad5137f053d7b55bf74e
stephengrider <ste.grider@gmail.com>
2018-06-11 16:24:32 -0600

    break up build

---
+++ animation/lib/src/screens/home.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  Widget build(context) {}
+}
