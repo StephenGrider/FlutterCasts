45abdbb98c1c73a032b6baa1f9c322b112094e25
stephengrider <ste.grider@gmail.com>
2018-06-11 13:02:07 -0600

    builder on button

--- login_bloc/lib/src/screens/login_screen.dart
+++ login_bloc/lib/src/screens/login_screen.dart
@@ -13,7 +13,7 @@ class LoginScreen extends StatelessWidget {
           emailField(bloc),
           passwordField(bloc),
           Container(margin: EdgeInsets.only(top: 25.0)),
-          submitButton(),
+          submitButton(bloc),
         ],
       ),
     );
@@ -52,11 +52,16 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget submitButton() {
-    return RaisedButton(
-      child: Text('Login'),
-      color: Colors.blue,
-      onPressed: () {},
+  Widget submitButton(Bloc bloc) {
+    return StreamBuilder(
+      stream: bloc.submitValid,
+      builder: (context, snapshot) {
+        return RaisedButton(
+          child: Text('Login'),
+          color: Colors.blue,
+          onPressed: () {},
+        );
+      },
     );
   }
 }
