7922425238190efc00251a204d6da848f20360e4
stephengrider <ste.grider@gmail.com>
2018-06-11 12:37:00 -0600

    combine latest

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class Bloc extends Object with Validators {
   final _email = StreamController<String>();
@@ -8,6 +9,8 @@ class Bloc extends Object with Validators {
   // Add data to stream
   Stream<String> get email => _email.stream.transform(validateEmail);
   Stream<String> get password => _password.stream.transform(validatePassword);
+  Stream<bool> get submitValid =>
+      Observable.combineLatest2(email, password, (e, p) => true);
 
   // Change data
   Function(String) get changeEmail => _email.sink.add;
