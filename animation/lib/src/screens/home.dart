09b1df7e79a109c588ce76cbe69d6009053c02ea
stephengrider <ste.grider@gmail.com>
2018-06-12 15:33:05 -0600

    flap

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -44,6 +44,7 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
             children: [
               buildCatAnimation(),
               buildBox(),
+              buildLeftFlap(),
             ],
           ),
         ),
@@ -72,6 +73,14 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       height: 200.0,
       width: 200.0,
       color: Colors.brown,
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Container(
+      height: 10.0,
+      width: 125.0,
+      color: Colors.red,
     );
   }
 }
