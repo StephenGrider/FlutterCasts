f21fa91233d9172ab510e59972bcaae5a479a276
stephengrider <ste.grider@gmail.com>
2018-06-14 16:35:17 -0600

    individual fetch

--- news/lib/src/resources/news_api_provider.dart
+++ news/lib/src/resources/news_api_provider.dart
@@ -1,11 +1,23 @@
+import 'dart:convert';
 import 'package:http/http.dart' show Client;
+import '../models/item_model.dart';
+
+final _root = 'https://hacker-news.firebaseio.com/v0';
 
 class NewsApiProvider {
   Client client = Client();
 
-  fetchTopIds() {
-    client.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+  fetchTopIds() async {
+    final response = await client.get('$_root/topstories.json');
+    final ids = json.decode(response.body);
+
+    return ids;
   }
 
-  fetchItem() {}
+  fetchItem(int id) async {
+    final response = await client.get('$_root/item/$id.json');
+    final parsedJson = json.decode(response.body);
+
+    return ItemModel.fromJson(parsedJson);
+  }
 }
