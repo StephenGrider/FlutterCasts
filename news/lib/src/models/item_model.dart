461e970984c1d8d302d5cf81689710d4e3ef3c5f
stephengrider <ste.grider@gmail.com>
2018-06-14 14:51:35 -0600

    item model

---
+++ news/lib/src/models/item_model.dart
@@ -0,0 +1,15 @@
+class ItemModel {
+  final int id;
+  final bool deleted;
+  final String type;
+  final String by;
+  final int time;
+  final String text;
+  final bool dead;
+  final int parent;
+  final List<dynamic> kids;
+  final String url;
+  final int score;
+  final String title;
+  final int descendants;
+}
