86fadd1a86106333f03a8fa211a57cb8369e20a2
stephengrider <ste.grider@gmail.com>
2018-06-20 13:33:19 -0600

    storiesbloc

--- news/lib/src/blocs/stories_bloc.dart
+++ news/lib/src/blocs/stories_bloc.dart
@@ -1,1 +1,14 @@
-class StoriesBloc {}
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class StoriesBloc {
+  final _topIds = PublishSubject<List<int>>();
+
+  // Getters to Streams
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  dispose() {
+    _topIds.close();
+  }
+}
