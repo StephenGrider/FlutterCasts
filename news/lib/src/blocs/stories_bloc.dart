b272c145fb2703ef9f38f80d63d19fcce630e9cd
stephengrider <ste.grider@gmail.com>
2018-06-20 13:42:12 -0600

    bloc getters

--- news/lib/src/blocs/stories_bloc.dart
+++ news/lib/src/blocs/stories_bloc.dart
@@ -3,10 +3,16 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
+  final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
 
   // Getters to Streams
   Observable<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
 
   dispose() {
     _topIds.close();
