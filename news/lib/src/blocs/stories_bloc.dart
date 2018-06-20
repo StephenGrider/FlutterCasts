aa4de88131f6c739e11bdaf7d892d45d75c75880
stephengrider <ste.grider@gmail.com>
2018-06-20 15:34:02 -0600

    single transformer

--- news/lib/src/blocs/stories_bloc.dart
+++ news/lib/src/blocs/stories_bloc.dart
@@ -6,9 +6,19 @@ import '../resources/repository.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
+
+  Observable<Map<int, Future<ItemModel>>> items;
 
   // Getters to Streams
   Observable<List<int>> get topIds => _topIds.stream;
+
+  // Getters to Sinks
+  Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -17,12 +27,16 @@ class StoriesBloc {
 
   _itemsTransformer() {
     return ScanStreamTransformer(
-      (Map<int, Future<ItemModel>> cache, int id, _) {},
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
       <int, Future<ItemModel>>{},
     );
   }
 
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
