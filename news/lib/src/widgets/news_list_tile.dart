bc5b9318f9a8bc53ba782bcdc946a16832f27991
stephengrider <ste.grider@gmail.com>
2018-06-20 16:57:20 -0600

    streambuilder

---
+++ news/lib/src/widgets/news_list_tile.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+import '../blocs/stories_provider.dart';
+
+class NewsListTile extends StatelessWidget {
+  final int itemId;
+
+  NewsListTile({this.itemId});
+
+  Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+
+    return StreamBuilder(
+      stream: bloc.items,
+      builder: (context, AsyncSnapshot<Map<int, Future<ItemModel>>> snapshot) {
+        if (!snapshot.hasData) {
+          return Text('Stream still loading');
+        }
+      },
+    );
+  }
+}
