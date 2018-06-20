7d3c2eca6f5f6e0bb82a68f77f39908aa01f176f
stephengrider <ste.grider@gmail.com>
2018-06-20 16:44:29 -0600

    listview cache

--- news/lib/src/screens/news_list.dart
+++ news/lib/src/screens/news_list.dart
@@ -30,6 +30,8 @@ class NewsList extends StatelessWidget {
         return ListView.builder(
           itemCount: snapshot.data.length,
           itemBuilder: (context, int index) {
+            bloc.fetchItem(snapshot.data[index]);
+
             return Text('${snapshot.data[index]}');
           },
         );
